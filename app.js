971f78b182a72102bd113522a9e96d766e7ccd6b
aishcann <aish.cann@gmail.com>
2021-04-29 22:39:09 -0400

    initial html setup, styling, and data capture in js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('hi')
+const questions = [
+    {question: 'question one here',
+    answer: 'answer one here'},
+    {question: 'question two here',
+    answer: 'answer two here'},
+    {question: 'question three here',
+    answer: 'answer three here'},
+    {question: 'question four here',
+    answer: 'answer four here'},
+    {question: 'question five here',
+    answer: 'answer five here'},
+]
+// console.log(questions)
+
+const startGame = document.querySelector('.start-game')
+// console.log(startGame)
+const startOver = document.querySelector('.start-over')
+// console.log(startOver)
+const submit = document.querySelector('.submit')
+// console.log(submit)
+const questionText = document.querySelector('.question-text')
+// console.log(questionText)
+const results = document.querySelector('.results')
+// console.log(results)
